591364f0f3efcac359867867a4c42360052f8b2e
ミヤガワショウマ <k024g1043@g.neec.ac.jp>
2025-08-09 17:19:24 +0900

    2-8

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -11,7 +11,7 @@ class Gamescene {};
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// エンジンの初期化
-	KamataEngine::Initialize(L"GC2D_04_オガワ_タクマ_AL3");
+	KamataEngine::Initialize(L"GC2D_07_ミヤガワ_AL3");
 
 	// DirectXcommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
